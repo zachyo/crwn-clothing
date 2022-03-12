e58a0f909841704d9c6105559e3809eaf51d54a8
zacheusIsrael <zacheazy99@yahoo.com>
2022-03-12 02:03:47 +0100

    broken homepage into smaller components, finished styling menu-item component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import Directory from "../../components/directory/directory.component";
+import "../homepage/homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
