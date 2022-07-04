7a21f91a2f4ea438df6d1c30e217539a24b1581e
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-04 22:41:52 +0100

    changed styles of custom button to workable one

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,14 +2,14 @@ import '../custom-button/custom-button.styles.scss';
 
 const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => {
     return (
-      <div
+      <button
         className={`${inverted ? "inverted" : ""} ${
           isGoogleSignIn ? "google-signin" : ""
         } custom-button`}
         {...otherProps}
       >
         {children}
-      </div>
+      </button>
     );
 }
  
