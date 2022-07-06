c8ae7edb454982f93d2c8cb17d6f74529343d4a0
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-06 22:19:44 +0100

    created new shop actions to monitor shop object

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,20 @@
-import shopActionTypes from './shop.types';
+import shopActionTypes from "./shop.types";
 
-export const updateCollections = (collectionsMap) => {
-  return {
-    type: shopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap,
-  };
-};
+export const updateCollections = (collectionsMap) => ({
+  type: shopActionTypes.UPDATE_COLLECTIONS,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsStart = () => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload : errorMessage
+});
